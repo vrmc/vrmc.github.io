8439ff736639bd43600ea4b9fc1f25cdb0be88e9
hnin <76209706+hninsatphyusin@users.noreply.github.com>
2023-01-13 12:18:58 +0800

    Update index.js

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,7 +1,7 @@
-var name = document.getElementsById("name").value;
-var email = document.getElementsById("email").value;
-var message = document.getElementsById("message").value;
-var subject = document.getElementsById("subject").value;
+var name = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+var message = document.getElementById("message").value;
+var subject = document.getElementById("subject").value;
 var url = "https://us-central1-eptest-1.cloudfunctions.net/sendEmail?name="+name+"&message="+message+"&email="+email+"&subject="+subject;
 document.getElementById('submit').action;
 document.getElementById('submit').action = url;
